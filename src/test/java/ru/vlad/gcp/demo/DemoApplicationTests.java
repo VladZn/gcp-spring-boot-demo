8e31e27c9ff76e896dfc01677bd7efa3135779b9
Vlad Zinchenko <v.zinchenko@itransition.com>
2021-05-21 00:43:58 +0200

    Initial commit

---
+++ src/test/java/ru/vlad/gcp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlad.gcp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
